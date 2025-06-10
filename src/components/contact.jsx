89ed84ebb08620af89b9b61bd726785a8fd4859d
--replace-all <rmeji3@uic.edu>
2025-06-09 21:03:49 -0700

    fixing footer

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -6,7 +6,10 @@ const Contact = ({contactRef}) => {
     return (
         <div ref={contactRef} className="h-[170px] bg-[#446E67] flex items-center justify-center">
             <div className="max-w-4xl px-4">
-                <div className="text-2xl text-[#fbf7f5] font-bold">Contact us</div>
+                <div className="">
+                    <div className="text-2xl text-[#fbf7f5] font-bold">Innovations Sanchez</div>
+                </div>
+                
                 <div className="flex items-center text-[#fbf7f5] mt-4">
                     <img src={emailIcon} className="h-6 mr-2" alt="Email logo" />
                     <a href="mailto:email@example.com">email@example.com</a>
