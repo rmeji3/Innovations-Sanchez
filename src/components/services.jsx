ed6b0eea976c70158f12300835b206a5b49f8cd9
Francisco <fsanc38@uic.edu>
2025-06-09 23:43:38 -0500

    services and ab9out us

--- src/components/services.jsx
+++ src/components/services.jsx
@@ -7,23 +7,23 @@ const Services = ({servicesRef}) => {
         <h1 className = "text item-center font-norm h-[190px] pt-27 text-green-800"> Our Services</h1>
         <div className ="grid grid-cols-1 sm:grid-cols-3 gap-6 m-3 ">
             <h2 className="text-center shadow-lg text-4xl font-bold text-[#446E67] border rounded m-3 pt-5 h-52 hover:shadow-2xl transition-shadow duration-300">
-                Lawn Mowing
+                Edging
                 <p className = "text-lg text-black mt-7 text-center font-normal m-4"> 
-                We provide professional lawn mowing services to keep your yard looking pristine. 
+                 Our edging services create clean lines and defined borders for your garden beds and walkways.
                 </p>
             </h2>
 
             <h2 className = "text-center shadow-lg text-4xl font-bold text-[#446E67] border rounded m-3 pt-5 h-52 hover:shadow-2xl transition-shadow duration-300">
-                Side Trimming
+              Lawn Mowing
             <p className = "text-lg text-black mt-7 text-center font-normal m-4">
-                Our side trimming keeps hedges and bushes neat, boosting your landscape’s look.
+                 We provide professional lawn mowing services to keep your yard looking pristine. 
             </p>
             </h2>
 
             <h2 className = "text-center shadow-lg text-4xl font-bold text-[#446E67] border rounded m-3 pt-5 h-52 hover:shadow-2xl transition-shadow duration-300">     
-                Weed Control
+                Bush Trimming
             <p className = "text-lg text-black mt-7 text-center font-normal m-4">
-                We offer effective weed control solutions to keep your garden healthy and free from unwanted plants.
+                We expertly trim bushes to enhance your garden’s aesthetics and health.
             </p>
             </h2>
         </div>
